e4118d37dc68391240963651908971d51f5748f9
amir1226 <amir1226@gmail.com>
2026-06-02 22:12:36 -0500

    BFS algorithm

---
+++ software-engineering/DSA/graph&trees/3.BFS/BFS_algorithm.py
@@ -0,0 +1,61 @@
+# Breadth-first algorithm
+from collections import deque
+
+def bfs_algorithm(graph, current_node=0):
+    q = deque()
+    q.append(current_node)
+    visited=[False]*len(graph)
+    visited[current_node] = True          # marca al encolar
+
+    while q:
+        current = q.popleft()
+        print(current)        
+        for node in graph[current]:
+            if not visited[node]:
+                q.append(node)
+                visited[node] = True
+
+    
+
+
+if __name__ == "__main__":
+    # Adjacency list:
+    #   0 — 1, 9
+    #   1 — 0, 8
+    #   2 — 3
+    #   3 — 2, 4, 5, 7
+    #   4 — 3
+    #   5 — 3, 6
+    #   6 — 5, 7
+    #   7 — 3, 6, 8, 10, 11
+    #   8 — 1, 3, 7, 9
+    #   9 — 0, 8
+    #  10 — 7, 11
+    #  11 — 7, 10
+    N = 12
+
+    edges = [
+        (0, 1),
+        (0, 9),
+        (1, 8),
+        (9, 8),
+        (8, 7),
+        (8, 3),
+        (3, 2),
+        (3, 4),
+        (3, 5),
+        (3, 7),
+        (5, 6),
+        (6, 7),
+        (7, 10),
+        (7, 11),
+        (10, 11),
+    ]
+
+    # Build adjacency list
+    adj = [[] for _ in range(N)]
+    for u, v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    bfs_algorithm(adj)
